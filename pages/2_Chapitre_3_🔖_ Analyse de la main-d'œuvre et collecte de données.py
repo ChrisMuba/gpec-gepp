3009fddbaeed131760925ea4c6916ec70587a82d
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-15 00:02:39 +0200

    Add files via upload

---
+++ pages/2_Chapitre_3_🔖_ Analyse de la main-d'œuvre et collecte de données.py
@@ -0,0 +1,152 @@
+
+
+
+import streamlit as st
+
+st.sidebar.success("Cliquez sur un :blue[chapitre] ou un :blue[quiz]")
+
+st.markdown("# Chapitre 2")
+st.sidebar.markdown("# Chapitre 2")
+
+st.title("Analyse de la main-d'œuvre et collecte de données")
+
+st.markdown("Une planification efficace des effectifs stratégiques de la main-d'œuvre commence par une **analyse complète des effectifs actuels** de votre organisation. Cette analyse implique la collecte et l'évaluation de données pour mieux comprendre les compétences, les performances et le potentiel de vos employés.") 
+
+st.markdown("Il s'agit d'une **étape critique pour comprendre les forces, les faiblesses et les domaines de votre organisation qui doivent être améliorés**.") 
+
+st.markdown("Ce chapitre décrit les principaux éléments de l'analyse des effectifs et de la collecte de données.") 
+
+st.markdown("")
+
+if st.button("Cliquez pour acceder au Chap.2 - **A/ Collecte et analyse des données de la main-d'œuvre**"):
+    
+    st.subheader("📈Chap.2-A/ Collecte et analyse des données📉")
+    
+    st.markdown("**Les données constituent le fondement de l'analyse des effectifs**. Les RH doivent collecter et analyser divers types de données pour avoir une **vue d'ensemble de la main-d'œuvre**.") 
+    
+    st.markdown("Ces données peuvent inclure:") 
+    
+    st.markdown("- **Information démographique** : Données sur l'âge, le genre et d'autres facteurs démographiques qui peuvent fournir des informations sur la composition de la main-d'œuvre.") 
+    
+    st.markdown("- **Compétences et qualifications des employés** : Informations sur la formation, les certifications et les qualifications des employés, qui aident à évaluer les niveaux et le potentiel de compétences.")
+
+    st.markdown("- **Métriques de performance** : Données sur les performances individuelles et en équipe, y compris les principaux indicateurs de performance (KPI) et les paramètres de mesure pertinents pour chaque rôle.")
+
+    st.markdown("- **Données sur le turnover et la rétention des talents** : informations sur les taux de rotation du personnel, les raisons de départ et les stratégies de fidélisation.")
+
+    st.markdown("- **Données sur l'engagement des employés** : Enquêtes et retour d'information qui évaluent l'engagement, la satisfaction et la motivation des salariés.")
+
+    st.markdown("- **Rémunération et avantages** : Données relatives aux salaires et avantages sociaux qui peuvent aider à déterminer si votre organisation est compétitive sur le marché du travail.")
+
+    st.markdown("")
+    
+    st.markdown("Une fois les données collectées, il faut les analyser pour identifier les modèles, les tendances et les domaines qui nécessitent une attention particulière.") 
+    
+    st.markdown("Des outils analytiques, des logiciels et des méthodes statistiques peuvent être utilisés pour donner un sens aux données et tirer des conclusions significatives.")
+
+    st.markdown("Pour en apprendre plus sur l'**analyse de données RH**, vous pouvez suivre notre :blue[**Cours de Statistiques appliquées aux Ressources Humaines**] : ") 
+
+    st.markdown("en cliquant ici ⬇️")
+                
+    st.link_button("Cours de Statistiques appliquées aux RH", "https://cours-stats-rh.streamlit.app/")
+
+    st.markdown("")
+
+    st.markdown(":blue[**Ou continuer vers la suite du chapitre 2**]")
+
+    st.markdown("en cliquant ici ⬇️")
+
+
+        
+# Add the "made with ❤️ by ..." text in the sidebar
+with st.sidebar:
+    st.write("Made with ❤️ by Chris MUBA")
+with st.sidebar:
+        st.image('GIF/take_notes.gif')
+
+if st.button("Continuer vers la suite du Chap.2 - **B/ Détermination des principaux indicateurs de performance**"):
+    
+    st.subheader("📈Chap.2-B/ Détermination des principaux KPI📉")
+    
+    st.markdown("Les indicateurs clés de performance (KPI) sont des indicateurs critiques qui mesurent la performance et l'efficacité de votre personnel. L'identification et le suivi des bons indicateurs clés de performance sont essentielles pour prendre des décisions éclairées au cours du processus de planification stratégique de la main-d'œuvre.") 
+    
+    st.markdown("Les indicateurs clés de performance communs pour l'analyse des effectifs sont les suivants :")
+    
+    st.markdown("- **Productivité des travailleurs** : Mesure de la production ou de la valeur générée par les employés dans un délai déterminé.")
+
+    st.markdown("- **Satisfaction et engagement des travailleurs** : évaluer le degré de satisfaction et d'engagement des employés au sein de l'organisation.")
+
+    st.markdown("- **Taux de turnover** : suivi du pourcentage d'employés qui quittent l'organisation volontairement ou involontairement.")
+
+    st.markdown("- **Délai de pourvoi** : Mmesurer le temps nécessaire pour pourvoir les postes vacants au sein de l'organisation.")
+
+    st.markdown("- **Lacunes en matière de compétences** : identifier les écarts entre les compétences dont une organisation a besoin et celles que possède son personnel.")
+
+    st.markdown("- **Pipeline de succession** : évaluer l'état de préparation des talents internes à assumer des rôles critiques à l'avenir.")
+
+    st.markdown("")
+
+    st.markdown("La sélection et le suivi des KPI qui correspondent aux objectifs stratégiques de votre organisation sont essentiels pour suivre les progrès et identifier les domaines qui nécessitent une intervention.")
+
+    st.markdown("")
+
+
+if st.button("Continuer vers la suite du Chap.2 - **C/ Réaliser une analyse des écarts de compétences**"):
+    
+    st.subheader("📈Chap.2-C/ Analyse des écarts de compétences📉")
+    
+    st.markdown("Une analyse des écarts de compétences est un processus systématique d'évaluation des aptitudes, des connaissances et des compétences de votre main-d'œuvre par rapport aux compétences requises pour atteindre vos objectifs stratégiques.") 
+    
+    st.markdown("Cette analyse vous aide à identifier les pénuries de compétences et guide vos efforts pour y remédier. Les étapes impliquées dans la réalisation d’une analyse des déficits de compétences comprennent :")
+    
+    st.markdown("- **Définir les compétences requises** : écrivez clairement les aptitudes et les compétences requises pour atteindre vos objectifs stratégiques.")
+
+    st.markdown("- **Évaluez les compétences actuelles** : évaluez les compétences et les capacités existantes de votre main-d'œuvre.")
+
+    st.markdown("- **Identifier les lacunes en matière de compétences** : comparez les compétences requises avec les compétences actuelles, en mettant en évidence les lacunes.")
+
+    st.markdown("- **Élaborer des plans de développement des compétences** : créer des plans de formation et de développement individuels ou en groupe pour combler les lacunes identifiées.")
+
+    st.markdown("")
+
+    st.markdown("Une analyse des déficits de compétences garantit que la main-d'œuvre de votre organisation est prête à répondre aux demandes de l'avenir, vous permettant ainsi d'allouer plus efficacement les ressources à la formation, au recrutement et au développement.")
+
+    st.markdown("")
+
+    st.markdown("En définitive, l’analyse des effectifs et la collecte de données sont en essentielles à une planification stratégique des effectifs réussie. En collectant et en analysant des données, en identifiant des KPI et en effectuant des analyses des lacunes en matière de compétences, il sera possible de prendre des décisions éclairées et garantir que la main-d'œuvre de l'organisation s'aligne sur ses buts et objectifs stratégiques.")
+
+    
+
+    st.markdown("")
+
+
+    def redirect_button(url: str, text: str= None, color="#FD504D"):
+        st.markdown(
+        f"""
+        <a href="{url}" target="_blank">
+            <div style="
+                display: inline-block;
+                padding: 0.5em 1em;
+                color: #FFFFFF;
+                background-color: {color};
+                border-radius: 3px;
+                text-decoration: none;">
+                {text}
+            </div
+        </a>
+        """,
+        unsafe_allow_html=True
+        )
+    redirect_button("https://gpec-gepp.streamlit.app/Quiz_2_-_Analyse_de_la_main-d'œuvre_et_collecte_de_données📉","Quiz du chapitre 2")
+
+    
+    
+
+
+
+        
+        
+        
+        
+        
+        
