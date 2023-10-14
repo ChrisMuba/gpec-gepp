4c5541f35f541dc62dbf483d918e5d114b64d74b
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-14 13:33:52 +0200

    Update 2_Chapitre_2_🔖_ Analyse de la main-d'œuvre et collecte de données.py

--- pages/2_Chapitre_2_🔖_ Analyse de la main-d'œuvre et collecte de données.py
+++ pages/2_Chapitre_2_🔖_ Analyse de la main-d'œuvre et collecte de données.py
@@ -44,7 +44,7 @@ if st.button("Cliquez pour acceder au Chap.2 - **A/ Collecte et analyse des donn
     
     st.markdown("Des outils analytiques, des logiciels et des méthodes statistiques peuvent être utilisés pour donner un sens aux données et tirer des conclusions significatives.")
 
-    st.markdown("Pour en apprendre plus en analyse de données RH, vous pouvez visiter notre :blue[**cours de Statistiques appliquées aux Ressources Humaines**] 
+    st.markdown("Pour en apprendre plus en analyse de données RH, vous pouvez visiter notre :blue[**cours de Statistiques appliquées aux Ressources Humaines**]") 
 
         st.link_button("Cours de Statistiques appliquées aux RH", "https://cours-stats-rh.streamlit.app/")
 
