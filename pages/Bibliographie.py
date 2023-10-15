8b43b12024756a1f548959bfbd5e7592317e0bd1
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-16 00:33:15 +0200

    Update Bibliographie.py

--- pages/Bibliographie.py
+++ pages/Bibliographie.py
@@ -11,25 +11,25 @@ with st.sidebar:
 
 st.title('Bibliographie / Webographie')
 
-st.markdown("*A faster way to build and share data apps* https://streamlit.io")
+#st.markdown("*A faster way to build and share data apps* https://streamlit.io")
 
-st.markdown("*Low-Code Data Apps* https://plotly.com")
+#st.markdown("*Low-Code Data Apps* https://plotly.com")
 
-st.markdown("*High-level interface for data visualization* https://plotly.com/python-api-reference/plotly.express.html")
+#st.markdown("*High-level interface for data visualization* https://plotly.com/python-api-reference/plotly.express.html")
 
-st.markdown("*Graph Objects in Python* https://plotly.com/python/graph-objects")
+#st.markdown("*Graph Objects in Python* https://plotly.com/python/graph-objects")
 
-st.markdown("*Python Data Analysis Library* https://pandas.pydata.org")
+#st.markdown("*Python Data Analysis Library* https://pandas.pydata.org")
 
-st.markdown("*The fundamental package for scientific computing with Python* https://numpy.org") 
+#st.markdown("*The fundamental package for scientific computing with Python* https://numpy.org") 
 
-st.markdown("*Statistical functions (scipy.stats)* https://docs.scipy.org/doc/scipy/reference/stats.html")
+#st.markdown("*Statistical functions (scipy.stats)* https://docs.scipy.org/doc/scipy/reference/stats.html")
 
-st.markdown("*Simple and efficient tools for predictive data analysis* https://scikit-learn.org/stable/index.html")
+#st.markdown("*Simple and efficient tools for predictive data analysis* https://scikit-learn.org/stable/index.html")
 
-st.markdown("*The Python Imaging Library* https://pillow.readthedocs.io")
+#st.markdown("*The Python Imaging Library* https://pillow.readthedocs.io")
 
-st.markdown("*Statistics: Power from Data !* https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-eng.htm")
+#st.markdown("*Statistics: Power from Data !* https://www150.statcan.gc.ca/n1/edu/power-pouvoir/toc-tdm/5214718-eng.htm")
 
 st.balloons()
 
