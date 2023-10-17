3af21385e979f3e5fcba4a606bfab970bc6f10c3
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-18 00:05:16 +0200

    Update 7_📚Quiz 7 - Suivi et évaluation.py

--- pages/7_📚Quiz 7 - Suivi et évaluation.py
+++ pages/7_📚Quiz 7 - Suivi et évaluation.py
@@ -6,57 +6,55 @@ import streamlit as st
 
 st.sidebar.success("Cliquez sur un :blue[chapitre] ou un :blue[quiz]")
 
-st.markdown("# Quiz du chapitre 2")
-st.sidebar.markdown("# Quiz du chapitre 2")
+st.markdown("# Quiz du chapitre 7")
+st.sidebar.markdown("# Quiz du chapitre 7")
 # Add the "made with ❤️ by ..." text in the sidebar
 with st.sidebar:
     st.write("Made with ❤️ by Chris MUBA")
 with st.sidebar:
         st.image('GIF/analyze_this.gif')
 
-st.title("Statistiques descriptives")
+st.title("Suivi et Évaluation")
 
-st.markdown("Ces questions visent à évaluer la compréhension des concepts clés et des sujets abordés dans le cours de **Statistiques descriptives**.")
+st.markdown("Ces questions visent à évaluer la compréhension des concepts clés et des sujets abordés dans le cours de **Suivi et Évaluation**.")
 
 st.markdown("Cocher les bonnes réponses.")
 
-st.header("A/ Mesures de tendance centrale")
+st.subheader("1/ Pourquoi les KPI et autres métriques sont-ils cruciaux dans le suivi et l'évaluation d'un plan stratégique de main-d'œuvre ?")
 
-st.subheader("1/ Laquelle des mesures de tendance centrale suivantes est la plus couramment utilisée pour résumer des montants de salaires ?")
-
-check = st.checkbox("a) Mode", key="check1")
+check = st.checkbox("a) Pour fournir un moyen de classer les employés en fonction de leurs performances.", key="check1")
 if check:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
     
-check_2 = st.checkbox("b) Médiane", key="check2")
+check_2 = st.checkbox("b) Pour servir de référence pour comparer les performances des RH avec celles des autres services.", key="check2")
 if check_2:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
     
-check_3 = st.checkbox("c) Moyenne géométrique", key="check3")
+check_3 = st.checkbox("c) Pour se concentrer uniquement sur la performance financière et les marges bénéficiaires.", key="check3")
 if check_3:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
     
-check_4 = st.checkbox("d) Moyenne arithmétique", key="check4")
+check_4 = st.checkbox("d) Pour aider à mesurer l’alignement de la main-d’œuvre sur les objectifs stratégiques et à suivre les progrès.", key="check4")
 if check_4:
     st.write("👏🏾**Bonne réponse !** 👍🏾")
     
     
 
-st.subheader("2/ Quelle mesure de tendance centrale est la plus appropriée à utiliser lorsque les données incluent des valeurs aberrantes fortement asymétriques")
+st.subheader("2/ À quelle fréquence les évaluations périodiques des effectifs doivent-elles généralement avoir lieu pendant la mise en œuvre de la planification stratégique ?")
 
-check_5 = st.checkbox("a) Mode", key="check5")
+check_5 = st.checkbox("a) Mensuellement", key="check5")
 if check_5:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
     
-check_6 = st.checkbox("b) Médiane", key="check6")
+check_6 = st.checkbox("b) Cela dépend des besoins spécifiques de l'organisation", key="check6")
 if check_6:
     st.write("👏🏾**Bonne réponse !** 👍🏾")
     
-check_7 = st.checkbox("c) Moyenne arithmétique", key="check7")
+check_7 = st.checkbox("c) Annuellement", key="check7")
 if check_7:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
     
-check_8 = st.checkbox("d) Moyenne géométrique", key="check8")
+check_8 = st.checkbox("d) Aussi rarement que possible", key="check8")
 if check_8:
     st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
 
@@ -65,108 +63,30 @@ if check_8:
 ########################################################################################################
 
 
-st.header("B/ Mesures de la variabilité")
+st.subheader("3/ Quel est l’objectif principal de l’évaluation de l’efficacité d’un plan stratégique de main-d’œuvre ?")
 
-st.subheader("1/ Quelle est la formule pour calculer la variance d'un échantillon ?")
-
-check_9 = st.checkbox("a) somme des écarts à la moyenne / taille de l'échantillon")
+check_9 = st.checkbox("a) Évaluer l'impact du plan sur l'efficacité du service RH")
 
 if check_9:
    st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
 
-check_10 = st.checkbox("b) somme des carrés des écarts à la moyenne / taille de l'échantillon")
+check_10 = st.checkbox("b) Pour vérifier si le plan correspond au logo et à l'image de marque de l'organisation")
 
 if check_10:
    st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
 
-check_11 = st.checkbox("c) somme des carrés des écarts à la moyenne / (taille de l'échantillon - 1) ")
+check_11 = st.checkbox("c) Mesurer la contribution du plan à la réalisation des objectifs stratégiques et apporter les ajustements nécessaires")
 
 if check_11:
   st.write("👏🏾**Bonne réponse !** 👍🏾")
 
-check_12 = st.checkbox("d) somme des écarts à la moyenne / (taille de l'échantillon taille - 1)")
+check_12 = st.checkbox("d) Classer les employés en fonction de leurs performances")
 
 if check_12:
    st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
 
 
-st.subheader("2/ Quelle mesure de variabilité peut être utilisée pour synthétiser des données salariales ?")
-
-check_13 = st.checkbox("a) Écart-type")
-
-if check_13:
-   st.write("👏🏾**Bonne réponse !** 👍🏾")
-
-check_14 = st.checkbox("b) Variance")
-
-if check_14:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_15 = st.checkbox("c) Étendue")
-
-if check_15:
-  st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_16 = st.checkbox("d) Coefficient de variation")
-
-if check_16:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-
 ########################################################################################################
-
-
-st.header("C/ Techniques graphiques")
-
-st.subheader("1/ Quel type de graphique serait le plus approprié pour afficher la répartition des salaires dans une entreprise ?")
-
-check_17 = st.checkbox("a) Graphique à barres")
-
-if check_17:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_18 = st.checkbox("b) Graphique linéaire")
-
-if check_18:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_19 = st.checkbox("c) Graphique circulaire")
-
-if check_19:
-  st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_20 = st.checkbox("d) Histogramme")
-
-if check_20:
-   st.write("👏🏾**Bonne réponse !** 👍🏾")
-
-
-st.subheader("2/ Lequel des éléments suivants serait le moins utile pour afficher les données d'une manière qui permette une comparaison facile entre plusieurs groupes ou catégories ?")
-
-check_21 = st.checkbox("a) Boîte à moustaches")
-
-if check_21:
-  st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_22 = st.checkbox("a) Graphique à barres empilées")
-
-if check_22:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_23 = st.checkbox("b) Nuage de points")
-
-if check_23:
-   st.write("👏🏾**Bonne réponse !** 👍🏾")
-
-check_24 = st.checkbox("c) Carte thermique")
-
-if check_24:
-  st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
-
-check_25 = st.checkbox("d) Analyse de régression")
-
-if check_25:
-   st.write("💩💩pfff... **mauvaise réponse**🤪🤪")
 
 
 st.markdown("")
@@ -189,7 +109,7 @@ def redirect_button(url: str, text: str= None, color="#FD504D"):
         """,
         unsafe_allow_html=True
         )
-redirect_button("https://cours-stats-rh.streamlit.app/Chapitre_3_🔖_Introduction_aux_probabilités_et_distributions","Aller au chapitre 3")
+redirect_button("https://gpec-gepp.streamlit.app/Chapitre_7_🔖_Suivi_et_évaluation","Aller au chapitre 3")
 
 
 
