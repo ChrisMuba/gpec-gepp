2ab0cb0de937cf87f5ecd2b77a6ce666000adfbe
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-19 01:22:59 +0200

    Update 7_📚Quiz 7 - Suivi et évaluation.py

--- pages/7_📚Quiz 7 - Suivi et évaluation.py
+++ pages/7_📚Quiz 7 - Suivi et évaluation.py
@@ -12,7 +12,7 @@ st.sidebar.markdown("# Quiz du chapitre 7")
 with st.sidebar:
     st.write("Made with ❤️ by Chris MUBA")
 with st.sidebar:
-        st.image('GIF/analyze_this.gif')
+        st.image('GIF/bPDKEmlJ.gif')
 
 st.title("Suivi et Évaluation")
 
