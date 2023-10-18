1d16123e18e2a3608146321047e4ee9b75c3c135
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-19 01:09:44 +0200

    Update 4_Chapitre_4_🔖_ Segmentation et analyse de l’offre de main-d’œuvre.py

--- pages/4_Chapitre_4_🔖_ Segmentation et analyse de l’offre de main-d’œuvre.py
+++ pages/4_Chapitre_4_🔖_ Segmentation et analyse de l’offre de main-d’œuvre.py
@@ -45,7 +45,7 @@ if st.button("Cliquez pour acceder au Chap.4 - **A/ Catégoriser la main-d'œuvr
 with st.sidebar:
     st.write("Made with ❤️ by Chris MUBA")
 with st.sidebar:
-        st.image('GIF/take_notes.gif')
+        st.image('GIF/people.gif')
 
 if st.button("Continuer vers la suite du Chap.4 - **B/ Analyse de l'offre de main-d'œuvre (interne ou externe)**"):
     
