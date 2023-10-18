4416f2e3d59df74e9caff0d3ff007c65f0a3d953
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-10-19 01:21:59 +0200

    Update 4_📚Quiz 4 - Segmentation et analyse de l’offre de main-d’œuvre.py

--- pages/4_📚Quiz 4 - Segmentation et analyse de l’offre de main-d’œuvre.py
+++ pages/4_📚Quiz 4 - Segmentation et analyse de l’offre de main-d’œuvre.py
@@ -12,7 +12,7 @@ st.sidebar.markdown("# Quiz du chapitre 4")
 with st.sidebar:
     st.write("Made with ❤️ by Chris MUBA")
 with st.sidebar:
-        st.image('GIF/analyze_this.gif')
+        st.image('GIF/bPDKEmlJ.gif')
 
 st.title("Segmentation et analyse de l'offre de main-d'œuvre")
 
