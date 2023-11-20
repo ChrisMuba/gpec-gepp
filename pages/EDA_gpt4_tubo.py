0909bcf415143df4292547b3fe7cb2db0f2cc3bc
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-20 18:36:44 +0100

    Update EDA_gpt4_tubo.py

--- pages/EDA_gpt4_tubo.py
+++ pages/EDA_gpt4_tubo.py
@@ -20,16 +20,16 @@ st.write(df.isnull().sum())
 
 # 1. Turnover Trends Over Time: We can use plotly to create a line graph showing turnover rates over time. We'll need to calculate the turnover rate first.
 
-df['turnover_date'] = pd.to_datetime(df['departure_date'])
-df['recruitment_date'] = pd.to_datetime(df['recruitment_date'])
-df['tenure'] = (df['turnover_date'] - df['recruitment_date']).dt.days / 365
+df['turnover_date'] = pd.to_datetime(df['Date_de_départ'])
+df['Date_de_recrutement'] = pd.to_datetime(df['Date_de_recrutement'])
+df['tenure'] = (df['turnover_date'] - df['Date_de_recrutement']).dt.days / 365
 df['turnover'] = 1
 df_turnover = df.groupby('turnover_date')['turnover'].sum()
 fig = px.line(df_turnover, x=df_turnover.index, y='turnover', title='Turnover Trends Over Time')
 st.plotly_chart(fig)
 
 # 2. Analysis of Turnover Rates by Department: We can use a bar chart to compare turnover rates across different departments.
-df_turnover_by_dept = df.groupby('department')['turnover'].sum()
+df_turnover_by_dept = df.groupby('Service')['turnover'].sum()
 fig = px.bar(df_turnover_by_dept, x=df_turnover_by_dept.index, y='turnover', title='Turnover Rates by Department')
 st.plotly_chart(fig)
 
@@ -38,17 +38,17 @@ fig = px.histogram(df, x='tenure', title='Impact of Tenure on Turnover')
 st.plotly_chart(fig)
 
 # 4.Gender-Based Turnover Analysis: We can use a bar chart to compare turnover rates between genders.
-df_turnover_by_gender = df.groupby('gender')['turnover'].sum()
+df_turnover_by_gender = df.groupby('Genre')['turnover'].sum()
 fig = px.bar(df_turnover_by_gender, x=df_turnover_by_gender.index, y='turnover', title='Gender-Based Turnover Analysis')
 st.plotly_chart(fig)
 
 # 5. Impact of Position Held on Turnover: We can use a bar chart to compare turnover rates by position.
-df_turnover_by_position = df.groupby('position_held')['turnover'].sum()
+df_turnover_by_position = df.groupby('Poste_occupé')['turnover'].sum()
 fig = px.bar(df_turnover_by_position, x=df_turnover_by_position.index, y='turnover', title='Impact of Position Held on Turnover')
 st.plotly_chart(fig)
 
 # 6. Reasons for Departure and Their Trends: We can use a stacked bar chart to show the trends of different reasons for departure over time.
-df_departure_reasons = df.groupby('reason_for_departure')['turnover'].sum()
+df_departure_reasons = df.groupby('Raison_du_départ')['turnover'].sum()
 fig = px.bar(df_departure_reasons, x=df_departure_reasons.index, y='turnover', title='Reasons for Departure and Their Trends')
 st.plotly_chart(fig)
 
