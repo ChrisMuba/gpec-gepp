b44de690552366ba56a03ce433916e1f91943f95
ChrisMuba <63189970+ChrisMuba@users.noreply.github.com>
2023-11-20 18:29:51 +0100

    Update EDA_gpt4_tubo.py

--- pages/EDA_gpt4_tubo.py
+++ pages/EDA_gpt4_tubo.py
@@ -1,72 +1,57 @@
-import streamlit as st
+
+
+# let's start by importing the necessary libraries and loading your dataset. We'll use pandas for data manipulation, numpy for numerical operations, and streamlit and plotly for data visualization.
 import pandas as pd
+import numpy as np
+import streamlit as st
 import plotly.express as px
-from datetime import datetime
 
-# Load the dataset
-@st.cache
-def load_data():
-    data = pd.read_csv('turnover_data.csv', parse_dates=['Date_de_recrutement', 'Date_de_départ'])
-    # Correcting gender labels if necessary (assuming 'Homme' is male and 'Femme' is female)
-    data['Genre'] = data['Genre'].replace({'Homme': 'Male', 'Femme': 'Female'})
-    return data
+# Next, let's load your dataset. We'll use pandas' read_csv function to load your CSV data into a DataFrame.
+df = pd.read_csv('your_file.csv')
 
-df = load_data()
+# Before we proceed with the analysis, we should check the basic information about the dataset and handle any missing values. We can use the info and describe functions for this.
+st.write(df.info())
+st.write(df.describe())
 
-# Calculate tenure in days
-df['tenure'] = (df['Date_de_départ'] - df['Date_de_recrutement']).dt.days
+# We can also check for missing values and handle them accordingly.
+st.write(df.isnull().sum())
 
-# Calculate turnover rate
-def calculate_turnover(df):
-    # Assuming the dataset is for a specific period, e.g., one year
-    total_employees = len(df)
-    total_departures = df['Date_de_départ'].notna().sum()
-    turnover_rate = (total_departures / total_employees) * 100
-    return turnover_rate
+# Now, let's proceed with the EDA analysis.
 
-turnover_rate = calculate_turnover(df)
+# 1. Turnover Trends Over Time: We can use plotly to create a line graph showing turnover rates over time. We'll need to calculate the turnover rate first.
 
-# Title of the app
-st.title('HR Turnover Analysis')
+df['turnover_date'] = pd.to_datetime(df['departure_date'])
+df['recruitment_date'] = pd.to_datetime(df['recruitment_date'])
+df['tenure'] = (df['turnover_date'] - df['recruitment_date']).dt.days / 365
+df['turnover'] = 1
+df_turnover = df.groupby('turnover_date')['turnover'].sum()
+fig = px.line(df_turnover, x=df_turnover.index, y='turnover', title='Turnover Trends Over Time')
+st.plotly_chart(fig)
 
-# Display turnover rate
-st.write(f"Overall Turnover Rate: {turnover_rate:.2f}%")
+# 2. Analysis of Turnover Rates by Department: We can use a bar chart to compare turnover rates across different departments.
+df_turnover_by_dept = df.groupby('department')['turnover'].sum()
+fig = px.bar(df_turnover_by_dept, x=df_turnover_by_dept.index, y='turnover', title='Turnover Rates by Department')
+st.plotly_chart(fig)
 
-# Turnover Trends Over Time
-st.subheader('Turnover Trends Over Time')
-turnover_trends = df.set_index('Date_de_départ').resample('M').size()
-fig_turnover_trends = px.line(x=turnover_trends.index, y=turnover_trends.values, labels={'x': 'Month', 'y': 'Turnover Count'})
-st.plotly_chart(fig_turnover_trends)
+# 3. Impact of Tenure on Turnover: We can use a histogram to show the distribution of tenure at departure.
+fig = px.histogram(df, x='tenure', title='Impact of Tenure on Turnover')
+st.plotly_chart(fig)
 
-# Analysis of Turnover Rates by Department
-st.subheader('Turnover Rates by Department')
-turnover_by_dept = df.groupby('Service')['Matricule'].count().reset_index()
-fig_turnover_by_dept = px.bar(turnover_by_dept, x='Service', y='Matricule', labels={'Matricule': 'Turnover Count'})
-st.plotly_chart(fig_turnover_by_dept)
+# 4.Gender-Based Turnover Analysis: We can use a bar chart to compare turnover rates between genders.
+df_turnover_by_gender = df.groupby('gender')['turnover'].sum()
+fig = px.bar(df_turnover_by_gender, x=df_turnover_by_gender.index, y='turnover', title='Gender-Based Turnover Analysis')
+st.plotly_chart(fig)
 
-# Impact of Tenure on Turnover
-st.subheader('Impact of Tenure on Turnover')
-fig_tenure = px.histogram(df, x='tenure', nbins=30)
-st.plotly_chart(fig_tenure)
+# 5. Impact of Position Held on Turnover: We can use a bar chart to compare turnover rates by position.
+df_turnover_by_position = df.groupby('position_held')['turnover'].sum()
+fig = px.bar(df_turnover_by_position, x=df_turnover_by_position.index, y='turnover', title='Impact of Position Held on Turnover')
+st.plotly_chart(fig)
 
-# Gender-Based Turnover Analysis
-st.subheader('Gender-Based Turnover Analysis')
-turnover_by_gender = df.groupby('Genre')['Matricule'].count().reset_index()
-fig_gender = px.bar(turnover_by_gender, x='Genre', y='Matricule', labels={'Matricule': 'Turnover Count'})
-st.plotly_chart(fig_gender)
+# 6. Reasons for Departure and Their Trends: We can use a stacked bar chart to show the trends of different reasons for departure over time.
+df_departure_reasons = df.groupby('reason_for_departure')['turnover'].sum()
+fig = px.bar(df_departure_reasons, x=df_departure_reasons.index, y='turnover', title='Reasons for Departure and Their Trends')
+st.plotly_chart(fig)
 
-# Impact of Position Held on Turnover
-st.subheader('Impact of Position Held on Turnover')
-turnover_by_position = df.groupby('Poste_occupé')['Matricule'].count().reset_index()
-fig_position = px.bar(turnover_by_position, x='Poste_occupé', y='Matricule', labels={'Matricule': 'Turnover Count'})
-st.plotly_chart(fig_position)
 
-# Reasons for Departure and Their Trends
-st.subheader('Reasons for Departure and Their Trends')
-reasons_trends = df.groupby([df['Date_de_départ'].dt.to_period('M'), 'Raison_du_départ']).size().unstack().fillna(0)
-fig_reasons_trends = px.line(reasons_trends)
-st.plotly_chart(fig_reasons_trends)
 
-# Run the Streamlit app
-if __name__ == '__main__':
-    st.run()
+
